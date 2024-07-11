03e81db46dbd1544bd6294325e02adb3ffff385f
Cabanero11 <cabanero1111@gmail.com>
2024-07-11 14:04:09 +0200

    Mp3 / Mp4 y cambio de fuente + Focus

--- downloader.py
+++ downloader.py
@@ -42,10 +42,12 @@ def toggle_button_clicked():
     global download_function
     if toggle_var.get() == 1:
         download_function = download_audio
-        messagebox.showinfo("Selección", "Se descargarán archivos de audio (.mp3)")
+        toggle_button.config(image=audio_on_img)
+        toggle_text.set("Audio (.mp3)")
     else:
         download_function = download_video
-        messagebox.showinfo("Selección", "Se descargarán archivos de video (.mp4)")
+        toggle_button.config(image=video_on_img)
+        toggle_text.set("Video (.mp4)")
 
 # Función para manejar el evento del botón de Descargar
 def download_button_clicked():
@@ -64,20 +66,35 @@ root.geometry("400x200")
 root.resizable(False, False)
 root.configure(bg="#f0f0f0")  # Color de fondo similar al estilo de Windows 11
 
+estilo = ttk.Style()
+estilo.configure('BW.TLabel', font=('Helvetica', 14))
+
+estilo_boton = ttk.Style()
+estilo_boton.configure('my.TButton', font=('Helvetica', 12))
+
 # Etiqueta y campo de entrada para la URL del video
-url_label = ttk.Label(root, text="URL del video de YouTube:")
+url_label = ttk.Label(root, text="URL del video de YouTube:", style='BW.TLabel')
 url_label.pack(pady=(20, 5))
 url_entry = ttk.Entry(root, width=50)
 url_entry.pack(pady=5)
+url_entry.focus()
+
+
+
+# Imágenes para el botón de toggle
+audio_on_img = tk.PhotoImage(file="audio_on.png")
+video_on_img = tk.PhotoImage(file="video_on.png")
+
+toggle_text = tk.StringVar(value="Audio (.mp3)")
 
 # Botón de Toggle para elegir entre descargar audio o video
 toggle_var = tk.IntVar(value=1)  # Inicia con la opción de descargar audio (.mp3)
-toggle_button = ttk.Checkbutton(root, text="Descargar como audio (.mp3)", variable=toggle_var, onvalue=1, offvalue=0, command=toggle_button_clicked)
+toggle_button = ttk.Checkbutton(root, textvariable=toggle_text, variable=toggle_var, onvalue=1, offvalue=0, command=toggle_button_clicked, image=audio_on_img, compound="right")
 toggle_button.pack(pady=10)
 
 # Botón para iniciar la descarga
-download_button = ttk.Button(root, text="Descargar", command=download_button_clicked)
-download_button.pack(pady=(10, 20))
+download_button = ttk.Button(root, text="Descargar", command=download_button_clicked, style='my.TButton')
+download_button.pack(pady=(10, 10))
 
 # Ejecutar la aplicación
 root.mainloop()
